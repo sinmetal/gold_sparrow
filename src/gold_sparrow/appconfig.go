88f8cf7ebfa2d24bec850a18e3ad984243be21cf
Shingo Ishimura <metal.tie@gmail.com>
2015-05-13 17:13:24 +0900

    gofmt

--- src/gold_sparrow/appconfig.go
+++ src/gold_sparrow/appconfig.go
@@ -1,28 +1,28 @@
 package gold_sparrow
 
 import (
-    "encoding/json"
-    "fmt"
-    "net/http"
-    "time"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
 
-    "github.com/zenazn/goji/web"
+	"github.com/zenazn/goji/web"
 
-    "appengine"
-    "appengine/datastore"
-    "github.com/mjibson/goon"
+	"appengine"
+	"appengine/datastore"
+	"github.com/mjibson/goon"
 )
 
 type AppConfig struct {
-    Id           string    `datastore:"-" goon:"id" json:"id"`        // app-config-id 固定
-    ClientId     string    `json:"clientId" datastore:",noindex"`     // GCP Client Id
-    ClientSecret string    `json:"clientSecret" datastore:",noindex"` // GCP Client Secret
-    CreatedAt    time.Time `json:"createdAt"`                         // 作成日時
-    UpdatedAt    time.Time `json:"updatedAt"`                         // 更新日時
+	Id           string    `datastore:"-" goon:"id" json:"id"`        // app-config-id 固定
+	ClientId     string    `json:"clientId" datastore:",noindex"`     // GCP Client Id
+	ClientSecret string    `json:"clientSecret" datastore:",noindex"` // GCP Client Secret
+	CreatedAt    time.Time `json:"createdAt"`                         // 作成日時
+	UpdatedAt    time.Time `json:"updatedAt"`                         // 更新日時
 }
 
 const (
-    appConfigId = "app-config-id"
+	appConfigId = "app-config-id"
 )
 
 type AppConfigApi struct {
@@ -32,74 +32,74 @@ type AppConfigService struct {
 }
 
 func SetUpAppConfig(m *web.Mux) {
-    api := AppConfigApi{}
+	api := AppConfigApi{}
 
-    m.Post("/admin/api/1/config", api.Put)
+	m.Post("/admin/api/1/config", api.Put)
 }
 
 func (a *AppConfigApi) Put(c web.C, w http.ResponseWriter, r *http.Request) {
-    ac := appengine.NewContext(r)
-    g := goon.FromContext(ac)
+	ac := appengine.NewContext(r)
+	g := goon.FromContext(ac)
 
-    var appCon AppConfig
-    err := json.NewDecoder(r.Body).Decode(&appCon)
-    if err != nil {
-        ac.Infof("rquest body, %v", r.Body)
-        er := ErrorResponse{
-            http.StatusBadRequest,
-            []string{"invalid request"},
-        }
-        er.Write(w)
-        return
-    }
-    defer r.Body.Close()
+	var appCon AppConfig
+	err := json.NewDecoder(r.Body).Decode(&appCon)
+	if err != nil {
+		ac.Infof("rquest body, %v", r.Body)
+		er := ErrorResponse{
+			http.StatusBadRequest,
+			[]string{"invalid request"},
+		}
+		er.Write(w)
+		return
+	}
+	defer r.Body.Close()
 
-    appCon.Id = appConfigId
-    _, err = g.Put(&appCon)
-    if err != nil {
-        er := ErrorResponse{
-            http.StatusInternalServerError,
-            []string{err.Error()},
-        }
-        ac.Errorf(fmt.Sprintf("datastore put error : ", err.Error()))
-        er.Write(w)
-        return
-    }
+	appCon.Id = appConfigId
+	_, err = g.Put(&appCon)
+	if err != nil {
+		er := ErrorResponse{
+			http.StatusInternalServerError,
+			[]string{err.Error()},
+		}
+		ac.Errorf(fmt.Sprintf("datastore put error : ", err.Error()))
+		er.Write(w)
+		return
+	}
 
-    w.Header().Set("Content-Type", "application/json; charset=utf-8")
-    w.WriteHeader(http.StatusOK)
-    json.NewEncoder(w).Encode(appCon)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(appCon)
 }
 
 func (s *AppConfigService) Get(ac appengine.Context) (AppConfig, error) {
-    g := goon.FromContext(ac)
+	g := goon.FromContext(ac)
 
-    appCon := AppConfig{
-        Id: appConfigId,
-    }
+	appCon := AppConfig{
+		Id: appConfigId,
+	}
 
-    err := g.Get(&appCon)
-    return appCon, err
+	err := g.Get(&appCon)
+	return appCon, err
 }
 
 func (appCon *AppConfig) Load(c <-chan datastore.Property) error {
-    if err := datastore.LoadStruct(appCon, c); err != nil {
-        return err
-    }
+	if err := datastore.LoadStruct(appCon, c); err != nil {
+		return err
+	}
 
-    return nil
+	return nil
 }
 
 func (appCon *AppConfig) Save(c chan<- datastore.Property) error {
-    now := time.Now()
-    appCon.UpdatedAt = now
+	now := time.Now()
+	appCon.UpdatedAt = now
 
-    if appCon.CreatedAt.IsZero() {
-        appCon.CreatedAt = now
-    }
+	if appCon.CreatedAt.IsZero() {
+		appCon.CreatedAt = now
+	}
 
-    if err := datastore.SaveStruct(appCon, c); err != nil {
-        return err
-    }
-    return nil
+	if err := datastore.SaveStruct(appCon, c); err != nil {
+		return err
+	}
+	return nil
 }
--- src/gold_sparrow/gold_sparrow.go
+++ src/gold_sparrow/gold_sparrow.go
@@ -1,32 +1,33 @@
 package gold_sparrow
 
 import (
-    "encoding/json"
-    "errors"
-    "net/http"
+	"encoding/json"
+	"errors"
+	"net/http"
 
-    "github.com/zenazn/goji"
+	"github.com/zenazn/goji"
 )
 
 var (
-    ConflictKey = errors.New("datastore: conflict key")
+	ConflictKey = errors.New("datastore: conflict key")
 )
 
 type ErrorResponse struct {
-    Status   int
-    Messages []string
+	Status   int
+	Messages []string
 }
 
 func init() {
-    m := goji.DefaultMux
+	m := goji.DefaultMux
 
-    SetUpAppConfig(m)
+	SetUpAppConfig(m)
+	SetUpGoogleToken(m)
 
-    goji.Serve()
+	goji.Serve()
 }
 
 func (er *ErrorResponse) Write(w http.ResponseWriter) {
-    w.Header().Set("Content-Type", "application/json; charset=utf-8")
-    w.WriteHeader(er.Status)
-    json.NewEncoder(w).Encode(er.Messages)
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	w.WriteHeader(er.Status)
+	json.NewEncoder(w).Encode(er.Messages)
 }
